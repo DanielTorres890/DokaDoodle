96c456be50434176c6e3a879dc5372f542f90b54
DanielTorres890 <danieltorres258006@gmail.com>
2025-03-30 18:55:06 -0700

    finished ranged enemy behavior
    Now enemies will kite away from the player, making sure to not run into a wall and picking a different direction if it would. After failing to escape an enemy more than some amount of times they'll attack anyways

--- Assets/scripts/MainGameScript/NewCombatSystem/EnemyLogic/RangedEnemyBehavior.cs
+++ Assets/scripts/MainGameScript/NewCombatSystem/EnemyLogic/RangedEnemyBehavior.cs
@@ -1,24 +1,45 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RangedEnemyBehavior : BaseEnemyBehavior
 {
     public float kiteRange;
-    public bool inKiteRange;
+    public float kiteAmount;
+
+    public float Precision;
+    public float StopKitingDistance;
 
     
+    private int failedEscapeCounter;
+
+    [Tooltip("How many times the enemy will kite attempt to kite away before attacking anyways")]
+    public int failedEscapeAmount;
+
+
+    public bool inKiteRange;
+
+    public bool avoiding;
 
     public override void Update()
     {
-        
-        
+        if (myManager.CanMove())
+        {
+            
+            kiteFinish();
 
-        inKiteRange = Vector3.Distance(gameObject.transform.position, targetManager.gameObject.transform.position) < kiteRange;
+            if (avoiding) { return; }
 
-        if(inKiteRange) 
-        { 
-            KiteAway();
-            return;
+            inKiteRange = Vector3.Distance(gameObject.transform.position, targetManager.gameObject.transform.position) < kiteRange;
+
+            if (inKiteRange && failedEscapeCounter < failedEscapeAmount)
+            {
+                KiteAway();
+                return;
+            }
+            failedEscapeCounter = 0;
         }
+        
+        
         base.Update();
     }
 
@@ -41,9 +62,30 @@ public class RangedEnemyBehavior : BaseEnemyBehavior
     
     public void KiteAway()
     {
-        Vector3 direction = targetManager.gameObject.transform.position - transform.position;
-        Vector3 oppositeDirection = transform.position - direction;
+        Quaternion NOJANK = gameObject.transform.rotation;
 
-        agent.SetDestination(oppositeDirection);
+        gameObject.transform.LookAt(targetManager.gameObject.transform.position);
+        gameObject.transform.Rotate(Vector3.up, -Precision);
+        bool freeme = true;
+        while (freeme)
+        {
+            gameObject.transform.Rotate(Vector3.up, Precision);
+            freeme = NavMesh.Raycast(gameObject.transform.position, gameObject.transform.TransformDirection(Vector3.back) * kiteAmount, out NavMeshHit hit, NavMesh.AllAreas);
+        }
+
+        agent.SetDestination(gameObject.transform.TransformDirection(Vector3.back) * kiteAmount);
+        avoiding = true;
+        gameObject.transform.rotation = NOJANK;
+    }
+
+    private void kiteFinish()
+    {
+        if (avoiding && Vector3.Distance(gameObject.transform.position, agent.destination) < StopKitingDistance)
+        {
+
+            failedEscapeCounter += 1;
+            avoiding = false;
+           
+        }
     }
 }
