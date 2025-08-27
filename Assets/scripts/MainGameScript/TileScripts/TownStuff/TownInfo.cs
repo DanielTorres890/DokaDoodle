ef80fae75144c0404726669f66b123c95b43de04
DanielTorres890 <danieltorres258006@gmail.com>
2025-08-27 01:19:51 -0700

    pretty much nothing

--- Assets/scripts/MainGameScript/TileScripts/TownStuff/TownInfo.cs
+++ Assets/scripts/MainGameScript/TileScripts/TownStuff/TownInfo.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Town", menuName = "TownSystem/Town")]
 public class TownInfo : ScriptableObject
 {
     public int baseMoneyGeneration;
     public int maxMoneyLevel;
     public int upgradeCost;
+
 }
--- Assets/scripts/MainGameScript/TileScripts/TownStuff/TownInfoDataBase.cs
+++ Assets/scripts/MainGameScript/TileScripts/TownStuff/TownInfoDataBase.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Town Database", menuName = "DataBases/Towns")]
 public class TownInfoDataBase : GenericDataBase<TownInfo>
 {
     
