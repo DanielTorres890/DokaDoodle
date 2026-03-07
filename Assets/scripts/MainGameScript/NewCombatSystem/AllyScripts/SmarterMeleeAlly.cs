ceff94f3a344421d9856ac99536da96f723fac90
DanielTorres890 <danieltorres258006@gmail.com>
2026-03-07 00:08:05 -0800

    small balance changes
    its lowkey kinda weird to make spark not completely whoop everything while making apprentice able to reasonably kill a bunga

--- Assets/scripts/MainGameScript/NewCombatSystem/AllyScripts/SmarterMeleeAlly.cs
+++ Assets/scripts/MainGameScript/NewCombatSystem/AllyScripts/SmarterMeleeAlly.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 
 public class SmarterMeleeAlly : SmarterMeleeEnemy
 {
+
+    [SerializeField] private GameObject spawnedWeapon;
+    [SerializeField] private Transform WeaponTransform;
+
+
     [Rpc(SendTo.ClientsAndHost, InvokePermission = RpcInvokePermission.Everyone)]
     public override void AttackAnimRpc(int attackIndex)
     {
         selectedAttack = myManager.stats.attacks[attackIndex];
 
+        if (spawnedWeapon != null) { Destroy(spawnedWeapon); }
+
+        if (selectedAttack.weaponPrefab != null && WeaponTransform)
+        {
+            spawnedWeapon = Instantiate(selectedAttack.weaponPrefab, WeaponTransform);
+            spawnedWeapon.transform.localPosition = selectedAttack.weaponPosition;
+        }
 
         overrideController["DefaultStartUp"] = selectedAttack.startUpAnimation;
 
